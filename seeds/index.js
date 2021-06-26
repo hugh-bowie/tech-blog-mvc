ff8f7cfe69a9f6b7173680c5268375369353a0f7
Hugh Bowie IV <hughbowie@me.com>
2021-06-26 16:50:47 -0400

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -5,13 +5,19 @@ const seedDataBlog = require("./blog-seeds");
 const sequelize = require("../config/connection");
 
 const seedMVC = async () => {
-    await sequelize.sync({ force: true });
-    console.log("---------DATABASE SYNCED SUCCESSFULLY---------");
-    await seedDataUser();
-    console.log("---------USER DATA SEEDED---------");
-    await seedDataComment();
-    console.log("---------COMMENT DATA SEEDED---------");
-    await seedDataBlog();
-    console.log("---------BLOG DATA SEEDED---------");
+    try {
+        await sequelize.sync({ force: true });
+        console.log("---------DATABASE SYNCED SUCCESSFULLY---------");
+        await seedDataUser();
+        console.log("---------USER DATA SEEDED---------");
+        await seedDataComment();
+        console.log("---------COMMENT DATA SEEDED---------");
+        await seedDataBlog();
+        console.log("---------BLOG DATA SEEDED---------");
+    }
+    catch (e) {
+        console.log('error = ', e);
+        process.exit(1);
+    }
 };
 seedMVC();
