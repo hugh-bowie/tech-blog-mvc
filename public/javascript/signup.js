3583f080c13c5afe3415c52a5563c5b1f92ca05a
Hugh Bowie IV <hughbowie@me.com>
2021-06-27 23:49:44 -0400

    Update signup.js

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,32 +1,38 @@
 async function signupFormHandler(event) {
-    event.preventDefault();
+    try {
+        event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    const twitter = document.querySelector('#twitter-signup').value.trim();
-    const github = document.querySelector('#github-signup').value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
-            method: 'post',
-            body: JSON.stringify({
-                username,
-                email,
-                twitter,
-                github,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+        const username = document.querySelector('#username-signup').value.trim();
+        const email = document.querySelector('#email-signup').value.trim();
+        const password = document.querySelector('#password-signup').value.trim();
+        const twitter = document.querySelector('#twitter-signup').value.trim();
+        const github = document.querySelector('#github-signup').value.trim();
 
-        // check the response status
-        if (response.ok) {
-            console.log('success');
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
+        if (username && email && password) {
+            const response = await fetch('/api/users', {
+                method: 'post',
+                body: JSON.stringify({
+                    username,
+                    email,
+                    twitter,
+                    github,
+                    password
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            // check the response status
+            if (response.ok) {
+                console.log('success');
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText);
+            }
         }
+    } catch (e) {
+        console.log('error = ', e);
+        process.exit(1);
     }
 }
 
