1c5b1d406a8751fa621c829b693adf1c22c9b20b
Hugh Bowie IV <hughbowie@me.com>
2021-06-27 22:47:27 -0400

    Update and rename deletePost.js to delete-post.js

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,29 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+    try {
+        const id = window.location.toString().split('/')[
+            window.location.toString().split('/').length - 1
+        ];
+
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+            body: JSON.stringify({
+                post_id: id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
+        }
+    } catch (e) {
+        console.log('error = ', e);
+        process.exit(1);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
