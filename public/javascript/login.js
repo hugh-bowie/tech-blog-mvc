ed8b5fa6b088cf196fbe7c4ea1b21558fee56e2e
Hugh Bowie IV <hughbowie@me.com>
2021-06-27 22:48:39 -0400

    Update login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,24 +1,30 @@
 async function loginFormHandler(event) {
     event.preventDefault();
+    try {
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+        const email = document.querySelector('#email-login').value.trim();
+        const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+        if (email && password) {
+            const response = await fetch('/api/users/login', {
+                method: 'post',
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+                headers: { 'Content-Type': 'application/json' }
+            });
 
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
+            if (response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText);
+            }
         }
+
+    } catch (e) {
+        console.log('error = ', e);
+        process.exit(1);
     }
 }
 
