d55e84fc8fe3db09af9db3bcf837de4858123033
Hugh Bowie IV <hughbowie@me.com>
2021-06-27 23:07:27 -0400

    Update logout.js

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,13 +1,18 @@
 async function logout() {
-    const response = await fetch('/api/users/logout', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' }
-    });
+    try {
+        const response = await fetch('/api/users/logout', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' }
+        });
 
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert(response.statusText);
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    } catch (e) {
+        console.log('error = ', e);
+        process.exit(1);
     }
 }
 
