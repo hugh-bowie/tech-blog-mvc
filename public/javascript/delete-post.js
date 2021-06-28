4aaaf4c0df75d45b04ca08bcc009cfb5248428c9
Hugh Bowie IV <hughbowie@me.com>
2021-06-27 23:48:21 -0400

    Update delete-post.js

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,26 +1,30 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
+    try {
+        event.preventDefault();
 
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+        const id = window.location.toString().split('/')[
+            window.location.toString().split('/').length - 1
+        ];
 
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-        body: JSON.stringify({
-            post_id: id
-        }),
-        headers: {
-            'Content-Type': 'application/json'
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'DELETE',
+            body: JSON.stringify({
+                post_id: id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard/');
+        } else {
+            alert(response.statusText);
         }
-    });
-
-    if (response.ok) {
-        document.location.replace('/dashboard/');
-    } else {
-        alert(response.statusText);
+    } catch (e) {
+        console.log('error = ', e);
+        process.exit(1);
     }
-
 }
 
 document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
